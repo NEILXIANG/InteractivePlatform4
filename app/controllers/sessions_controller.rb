d9e3e34dd87112d056e8ef109c6230a5e7c24b62
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2013-11-18 11:57:59 +0800

    用户登录。

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if user = User.authenticate(params[:name], params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, :alert => "用户名或密码不正确"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to user_url, :notice => "已退出"
+  end
+end
