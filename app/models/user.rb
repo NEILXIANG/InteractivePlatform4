08fc227f035bef97edcc1977a8a46813ea5fed78
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2013-11-18 11:25:47 +0800

    用户注册-会员信息

---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+
+require 'digest/sha2'
+
+class User < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => true
+  validates :password, :confirmation => true
+  attr_accessor :password_confimation
+  attr_reader   :password
+
+  validate  :password_must_be_present
+
+  def User.authenticate(name, password)
+    if user = find_by_name(name)
+      if user.hashed_password == encrypt_password(password, user.salt)
+        user
+      end
+    end
+  end
+
+  def User.encrypt_password(password, salt)
+    Digest::SHA2.hexdigest(password + "InteractivePlatform4" + salt)
+  end
+
+  def password=(password)
+    @password = password
+
+    if password.present?
+      generate_salt
+      self.hashed_password = self.class.encrypt_password(password, salt)
+      if logger.debug?
+        logger.debug("hashed_password=" + hashed_password)
+      end
+    end
+  end
+
+  private
+
+  def password_must_be_present
+    errors.add(:password, "缺少密码") unless hashed_password.present?
+  end
+
+  def generate_salt
+    self.salt = self.object_id.to_s + rand.to_s
+  end
+end
