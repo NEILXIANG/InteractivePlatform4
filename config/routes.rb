9c45b60e7e7900f9032382f3ff73fbafd33491ef
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2013-11-19 10:51:21 +0800

    新增首页的导航界面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 InteractivePlatform4::Application.routes.draw do
+  get "welcome/index"
   get 'admin' => "admin#index"
 
   controller :sessions do
@@ -12,7 +13,7 @@ InteractivePlatform4::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
