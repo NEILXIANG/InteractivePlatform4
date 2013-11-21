4983bef23454ec0744713a0992ba5d370a197d9c
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2013-11-21 09:52:31 +0800

    贴子的校验的注释。

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, :content, :user_id, :presence => true
-  validates :title, :uniqueness => true
+  validates :title, :content, :user_id, :presence => true   # 标题，内容，作者id不能为空
+  validates :title, :uniqueness => true                     # 标题必须唯一
 end
