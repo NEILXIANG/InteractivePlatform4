cd810c0bf8a1bc80f1b53fcc71cb3fbaa94c1f21
Sheldon Chen <sheldon.ziping.chen@gmail.com>
2013-11-24 16:02:48 +0800

    添加对模型的基本单元测试

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
+# -*- coding: UTF-8 -*-
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # 属性不能为空
+  test "attributes must be no null" do
+    post = Post.new
+    assert !post.valid?
+  end
+
+  # 标题必须是唯一
+  test "unique title" do
+    post = Post.new(
+        :title      => posts(:one).title,
+        :content    => 'content',
+        :user_id    => 1
+      )
+    assert !post.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
+# -*- coding: UTF-8 -*-
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid with empty attributes" do
+    user = User.new
+    assert !user.valid?
+  end
+
+  test "unique name" do
+    sheldonchen = User.new(
+            :name       => users(:sheldonchen).name,
+            :password   => 'secret'
+        )
+    assert !sheldonchen.save
+  end
 end
